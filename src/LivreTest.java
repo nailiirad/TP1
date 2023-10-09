3b9575bf1fedeeac4563c87bfdceb2875b2e8094
HP <nailiiradri@gmail.com>
2023-10-09 10:19:22 +0100

    Initial commit

---
+++ src/LivreTest.java
@@ -0,0 +1,7 @@
+/*public class LivreTest {
+    public static void main(String[] args){
+        livre L1 = new livre("java","Ali", 500, 9);
+
+        System.out.println("le titre est : " + L1.titre);
+    }
+}*/
--- src/livre.java
+++ src/livre.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class livre {
+public class livre {
+    private String titre;
+    private String auteur;
+    private float prix;
+    private int ID;
+    public livre(String t, String a, float p, int id){
+        this.titre = t;
+        this.auteur = a;
+        this.prix = p;
+        this.ID = id;
+    }
+    public class LivreTest {
+        public static void main(String[] args){
+            livre L1 = new livre("java","Ali", 500, 9);
+            System.out.println("le titre est : " + L1.titre);
+        }
+    }
+
 }
