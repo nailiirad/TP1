3728bd40e97f6045670c571a90309cfbdea0f46d
HP <nailiiradri@gmail.com>
2023-10-09 15:07:59 +0100

    Initial commit

--- src/LivreTest.java
+++ src/LivreTest.java
@@ -1,7 +1,12 @@
-/*public class LivreTest {
+public class LivreTest {
     public static void main(String[] args){
-        livre L1 = new livre("java","Ali", 500, 9);
-
-        System.out.println("le titre est : " + L1.titre);
+        livre L1 = new livre("java","Ali", 50, 9);
+        L1.affiche();
+        L1.settitre("Python");
+        L1.gettitre();
+        L1.affiche();
+        livre L2 = new livre("PHP","Imed", 40, 8);
+        System.out.println("nombre de livre : " + livre.Nb);
+        System.out.println(L1);
     }
-}*/
+}
--- src/livre.java
+++ src/livre.java
@@ -3,17 +3,49 @@ public class livre {
     private String auteur;
     private float prix;
     private int ID;
+    static int Nb;
     public livre(String t, String a, float p, int id){
         this.titre = t;
         this.auteur = a;
         this.prix = p;
         this.ID = id;
+        Nb++;
     }
-    public class LivreTest {
+/*    public class LivreTest {
         public static void main(String[] args){
             livre L1 = new livre("java","Ali", 500, 9);
             System.out.println("le titre est : " + L1.titre);
         }
+    }*/
+    public void affiche() {
+        System.out.println("le titre est : " + this.titre);
     }
 
+    public void settitre(String t) {
+        this.titre = t;
+    }
+    public String gettitre() {
+        return this.titre;
+    }
+    public void setAuteur(String a) {
+        this.auteur = a;
+    }
+    public String getAuteur() {
+        return this.auteur;
+    }
+    public void setprix(float p) {
+        this.prix = p;
+    }
+    public float getprix() {
+        return this.prix;
+    }
+    public void setID(int id) {
+        this.ID = id;
+    }
+    public int getID() {
+        return this.ID;
+    }
+    public String toSting(){
+        return ("le livre est : " + this.titre);
+    }
 }
